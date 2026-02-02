01f79309bcf7ad4570a8af64025fcc8e3796b225
Oleksandr Melnyk <mullerbox@gmail.com>
2026-02-02 11:37:59 +0100

    Add `Auth` struct with Login, RegisterNewUser, and IsAdmin methods; introduce `UserSaver` and `UserProvider` contracts

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -1,1 +1,22 @@
 package auth
+
+import (
+	"context"
+)
+
+type Auth struct{}
+
+func (a Auth) Login(ctx context.Context, email, password string, appId int) (token string, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a Auth) RegisterNewUser(ctx context.Context, email, password string) (userID int64, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a Auth) IsAdmin(ctx context.Context, userID int64) (isAdmin bool, err error) {
+	//TODO implement me
+	panic("implement me")
+}
---
+++ internal/services/auth/contracts.go
@@ -0,0 +1,15 @@
+package auth
+
+import (
+	"context"
+	"sso/internal/domain/models"
+)
+
+type UserSaver interface {
+	SaveUser(ctx context.Context, email string, passHash []byte) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email int64) (models.User, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
